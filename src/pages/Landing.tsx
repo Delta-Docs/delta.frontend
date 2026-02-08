59c3b62427dbd066c0d11736073e4380389573b7
midhunann <midhunmidhunan@gmail.com>
2026-02-09 00:38:01 +0530

    refactor: replace static login button with Navbar component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,21 +1,11 @@
-import { Link } from 'react-router-dom'
-import { Button } from '@/components/shadcn/button'
 import { DeltaHero } from '@/components/landing/DeltaHero'
 import { AboutSection } from '@/components/landing/AboutSection'
+import { Navbar } from '@/components/landing/Navbar'
 
 export default function Landing() {
     return (
-        <div className="min-h-screen bg-[#0F2A44] flex flex-col">
-            {/* Login Button Overlay */}
-            <div className="absolute top-6 right-6 z-50">
-                <Link to="/login">
-                    <Button
-                        className="bg-[#7C94B8] text-white hover:bg-[#D9D9D8] hover:text-[#7C94B8] px-6 font-medium tracking-wide shadow-lg border-2 border-transparent hover:border-[#7C94B8]/50 transition-all"
-                    >
-                        Log in
-                    </Button>
-                </Link>
-            </div>
+        <div className="min-h-screen bg-deep-navy flex flex-col">
+            <Navbar />
 
             <main className="flex-grow">
                 <DeltaHero />
@@ -23,9 +13,9 @@ export default function Landing() {
             </main>
 
             {/* Copyright Footer */}
-            <footer className="py-6 bg-[#0F2A44] border-t border-[#7C94B8]/20 z-10 relative">
+            <footer className="py-6 bg-deep-navy border-t border-ocean-city/20 z-10 relative">
                 <div className="container mx-auto px-4 text-center">
-                    <p className="text-xs text-[#7C94B8]/60">
+                    <p className="text-xs text-ocean-city/60">
                         &copy; 2025 Delta. All rights reserved.
                     </p>
                 </div>
